a12e8f7d38c858dc5d7c5744e2aec7f8ec220108
DianaKun17 <49477276+DianaKun17@users.noreply.github.com>
2019-09-30 14:59:36 +0300

    Update and rename ДЗ_1.5.txt to ДЗ_1.5.py

--- ДЗ_1.5.py
+++ ДЗ_1.5.py
@@ -2,12 +2,12 @@ print("Vvedite znacheniya X and Y: ")
 x = float(input("X: "))
 y = float(input("Y: "))
 if x > 0 and y > 0:
-  print("1 chetvert")
+    print("1 chetvert")
 elif x > 0 and y < 0:
-  print("4 chetvert")
+    print("4 chetvert")
 elif x < 0 and y > 0:
-  print("2 chetvert")
+    print("2 chetvert")
 elif x < 0 and y < 0:
-  print("3 chetvert")
+    print("3 chetvert")
 else:
-  print("na osi")
+    print("na osi")
