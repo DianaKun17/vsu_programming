63f835dc03b078d013191ccb0b64fae1bfe4ea3d
DianaKun17 <49477276+DianaKun17@users.noreply.github.com>
2019-09-30 15:01:45 +0300

    Update ДЗ_1.1.py

--- ДЗ_1.1.py
+++ ДЗ_1.1.py
@@ -1,8 +1,8 @@
 a = float(input("Vvedite a "))
 b = float(input("Vvedite b "))
 if a > b:
-    print ("1e chislo bolshe 2go")
+    print("1e chislo bolshe 2go")
 elif a < b:
-    print ("2e chislo bolshe 1go")
+    print("2e chislo bolshe 1go")
 else:
-    print ("oni ravni")
+    print("oni ravni")
