0521e215b514230a17f27d224df071f5342a0b54
DianaKun17 <49477276+DianaKun17@users.noreply.github.com>
2019-09-30 15:02:24 +0300

    Update ДЗ_1.3.py

--- ДЗ_1.3.py
+++ ДЗ_1.3.py
@@ -2,4 +2,4 @@ import math
 x = float(input("Vvedite x "))
 y = float(input("Vvedite y "))
 z = float(input("Vvedite z "))
-print(math.sqrt(x ** 2 + y **2 + z **2))
+print(math.sqrt(x**2 + y**2 + z**2))
