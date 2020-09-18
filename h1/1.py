2a9078f71842381b3fd8a91d7dd97963ec187f97
DianaKun17 <49477276+DianaKun17@users.noreply.github.com>
2020-09-18 23:05:19 +0300

    Create 1.py

---
+++ h1/1.py
@@ -0,0 +1,11 @@
+a = float(input("Vvedite 1e chislo"))
+b = float(input("Vvedite 2e chislo")) 
+
+if a < b:
+    print("b bolshe a")
+elif a == b:
+    print("a ravno b)
+else:
+    print("a bolshe b"
+
+
