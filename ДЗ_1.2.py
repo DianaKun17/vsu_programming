5bf6457c0247f7eb97a54877abad1f702f34be44
DianaKun17 <49477276+DianaKun17@users.noreply.github.com>
2020-09-18 22:53:17 +0300

    Delete ДЗ_1.2.py

--- ДЗ_1.2.py
+++
@@ -1,9 +0,0 @@
-a = float(input("Vvedite a "))
-b = float(input("Vvedite b "))
-c = float(input("Vvedite c "))
-d = float(input("Vvedite d "))
-f = float(input("Vvedite f "))
-if f == d:
-    print("nelzya")
-else:
-    print(a * b - c) / (f - d)
