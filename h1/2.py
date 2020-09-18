c02f7a6a39c168aaa579ecd474af99423880529a
DianaKun17 <49477276+DianaKun17@users.noreply.github.com>
2020-09-18 23:10:08 +0300

    Create 2.py

---
+++ h1/2.py
@@ -0,0 +1,11 @@
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+f = float(input("f = "))
+d = float(input("d = "))
+k = f - d
+
+if k:
+    print((a * b - c) / (f - d))
+else:
+    print("Na 0 delit' nel'zya")
