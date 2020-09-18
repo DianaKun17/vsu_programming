a93097cc08770cc01049fcaf1051c5bb369db0ad
DianaKun17 <49477276+DianaKun17@users.noreply.github.com>
2020-09-18 23:50:31 +0300

    Create 2.py

---
+++ h3/2.py
@@ -0,0 +1,9 @@
+a = int(input("vvedite chislo mesyasa ")
+if (0 < a < 3) or (a==12):
+    pritn("zima")
+if 2 < a < 6:
+    print("Vesna")
+if 5 < a < 9:
+    print("Leto")
+if 8 < a < 12:
+    print("Osen'")
